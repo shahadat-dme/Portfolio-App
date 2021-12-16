c37a2d84057bebcc0e48faeacf2dc3848b268936
Shahadat <shahadat2cp17@gmail.com>
2021-12-16 11:13:41 +0600

    profile

---
+++ src/PortfoliContainer/Home/Profile.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+
+export default function Profile() {
+    return (
+        <div className='profile-container'>
+            <div className="profile-parent">
+                <div className="profile-details">
+                    <div className="colz">
+                        <a href='#'>
+                            <i className="fa fa-facebokk-square"></i>
+                        </a>
+                        <a href='#'>
+                            <i className="fa fa-linkedin"></i>
+                        </a>
+                        <a href='#'>
+                            <i className="fa fa-instagram"></i>
+                        </a>
+                        <a href='#'>
+                            <i className="fa fa-youtube-square"></i>
+                        </a>
+                        <a href='#'>
+                            <i className="fa fa-twitter"></i>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
